65b453e13853f15e62337b81477723fa25dd0bcb
cenzhongman <cenzhongman@163.com>
2018-12-03 10:55:30 +0800

    first commit

---
+++ src/main/java/com/cenzhongman/Demo.java
@@ -0,0 +1,11 @@
+package com.cenzhongman;
+
+/**
+ * @author 岑忠满
+ * @date 2018/12/3 10:54
+ */
+public class Demo {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
